39670b28581db759660f8a0bb3074bf50afa0201
Lucas Santos <leos9877@gmail.com>
2021-09-01 20:02:40 -0300

    :recycle: modificando nomes.

--- ex001 | IMC - JavaScript/app.js
+++
@@ -1,3 +0,0 @@
-const dados_json = require("./pessoas.json")
-
-for(let pessoa of dados_json.pessoas) console.log(`${pessoa.nome}\nIMC: ${(pessoa.peso / (pessoa.altura * pessoa.altura))}\n`)
---
+++ ex001/app.js
@@ -0,0 +1,2 @@
+const dados_json = require("./pessoas.json")
+for(let pessoa of dados_json.pessoas) console.log(`${ pessoa.nome }\nIMC: ${ pessoa.peso / (pessoa.altura * pessoa.altura) }\n`)
