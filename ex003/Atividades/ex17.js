ca87e8577b350177155d40359e8694224456aa4b
Lucas Santus <leos9877@gmail.com>
2021-09-03 17:24:48 -0300

    :pencil: atualizando codigos.

--- ex003/Atividades/ex17.js
+++ ex003/Atividades/ex17.js
@@ -1,12 +1,12 @@
-const pessoas = require('./Pessoas.json');
+const pessoas = require('../pessoas.json');
 let cont = 0 
-dados = pessoas.filter((posicao, objeto) => {
-    return objeto.nome == "Laura Eduarda da Cunha"
-});
+
 pessoas.forEach(i => {
     cont+=1
     if(i.nome=="Laura Eduarda da Cunha"){
         console.log(cont)
     }
 })
+
+console.log(cont)
 
