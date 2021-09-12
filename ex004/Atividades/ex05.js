1288e6dc713a35d634a6baebe18b3b558591a583
Lucas Santos <leos9877@gmail.com>
2021-09-12 11:47:17 -0300

    :pencil: aprimorando exercicio 05 da atividade 4.

--- ex004/Atividades/ex05.js
+++ ex004/Atividades/ex05.js
@@ -2,23 +2,27 @@
 const feriados = require('../Feriados.json');
 const redline = require('readline');
 
-// criando interface input.
-const rl = redline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function pesquisar(){
+    // criando interface input.
+    const rl = redline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-// Validando data.
-rl.question("Insira o Nome do Feriado: ", resposta =>{
-    is_valid = false;
+    // Validando data.
+    rl.question("Insira o Nome do Feriado: ", resposta =>{
+        is_valid = false;
 
-    for(feriado of feriados){
-        if(resposta == feriado.name) is_valid = true
-    }
+        for(feriado of feriados){
+            if(resposta == feriado.name) is_valid = true
+        }
 
-    // Printando.
-    is_valid ? console.log(`Feriado, Data: ${feriado.date}`) : console.log(`Não tem feriados!`);
+        // Printando.
+        is_valid ? console.log(`Feriado, Data: ${feriado.date}`) : console.log(`Não tem feriados!`);
 
-    // Fechando interface input.
-    rl.close();
-});
+        // Fechando interface input.
+        rl.close();
+    });
+}
+
+pesquisar();
