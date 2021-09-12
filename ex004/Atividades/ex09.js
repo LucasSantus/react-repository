609463d2e707f7950c5ba00a3f5ad09b49c88515
Lucas Santos <leos9877@gmail.com>
2021-09-12 11:47:17 -0300

    :pencil: aprimorando exercicio 09 da atividade 4.

--- ex004/Atividades/ex09.js
+++ ex004/Atividades/ex09.js
@@ -1,18 +1,14 @@
 // Importando veiculos.
 const veiculos = require('../Veiculos.json');
 
-// Declarando variaveis.
-let valor = 0;
-let car = ""
-
-// Verificando veiculo mais caro.
-veiculos.forEach(veiculo => {
-    valor_veiculo = parseFloat(veiculo.valor.replace("R$ ", ""));
-    if(valor < parseFloat(valor_veiculo)){
-        valor = parseFloat(valor_veiculo);
-        car = veiculo;
-    }
+// Ordenando veiculos por valor.
+i = veiculos.sort(function (a, b) {
+    convert_a = parseFloat(a.valor.replace("R$ ", ""));
+    convert_b = parseFloat(b.valor.replace("R$ ", ""));
+    if (convert_a > convert_b) { return 1; }
+    if (convert_a < convert_b) { return -1; }
+    return 0;
 });
 
 // Printando.
-console.log(`${car.modelo} é o carro mais caro, valendo: ${valor} reias.`);
+console.log(`O carro mais caro:\n${i[i.length - 1].marca} ${i[i.length - 1].modelo} - ${i[i.length - 1].valor}`);
