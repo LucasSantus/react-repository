8d2698195f1b8fa2a7f8ea9c783057f265ab1193
Lucas Santos <leos9877@gmail.com>
2021-09-12 11:47:17 -0300

    :pushpin: adicionando exercicio 21 da atividade 4.

---
+++ ex004/Atividades/ex21.js
@@ -0,0 +1,20 @@
+// Importando veiculos.
+const veiculos = require('../Veiculos.json');
+
+// Separando marcas.
+let list_fabricantes = []
+veiculos.forEach(i => { if(!list_fabricantes.includes(i.marca)){ list_fabricantes.push(i.marca); }});
+
+console.log(`O valor total dos carros por fabricante:`);
+
+// Filtrando e Printando fabricantes.
+list_fabricantes.forEach(fabricante => {
+    valor_total = 0.0;
+
+    fabricantes_filtrados = veiculos.filter(veiculo => { return veiculo.marca == fabricante; });
+
+    fabricantes_filtrados.forEach(a => { valor_total += parseFloat(a.valor.replace("R$ ", "")) });
+
+    // Printando.
+    console.log(`\n${fabricante} - ${valor_total}`);
+})
